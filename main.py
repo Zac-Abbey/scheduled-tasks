6fbbb44fb3b560906130429637580d08b6ce3246
ZACCHEUS ABIODUN <161876529+Zac-Abbey@users.noreply.github.com>
2026-07-04 13:59:21 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-# To run and test the code, you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explanations.
-
-
 import os
 from datetime import datetime
 import pandas
@@ -53,4 +45,7 @@ if today_tuple in birthday_dict:
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 
 # 4. Send the letter generated in step 3 to that person's email address.
+
+
+
 
